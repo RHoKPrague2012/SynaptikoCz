85b8865ae96cdb5783dca9ce60ab14c5006639d4
Jiri Prokop <jiri-prokop@synaptiko.cz>
2012-06-03 12:45:35 +0200

    Test method testDbHelper added.

--- BioadresarAndroid/src/cz/duha/bioadresar/MainTabbedActivity.java
+++ BioadresarAndroid/src/cz/duha/bioadresar/MainTabbedActivity.java
@@ -1,12 +1,18 @@
 package cz.duha.bioadresar;
 
-import cz.duha.bioadresar.data.DatabaseHelper;
+import java.io.IOException;
+import java.util.Hashtable;
+
+import android.app.ListActivity;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.database.SQLException;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TabHost;
+import cz.duha.bioadresar.data.DatabaseHelper;
+import cz.duha.bioadresar.data.FarmInfo;
 
 
 public class MainTabbedActivity extends TabActivity {
@@ -37,6 +43,17 @@ public class MainTabbedActivity extends TabActivity {
 	    			res.getDrawable(R.drawable.ic_launcher))
 	    		.setContent(intent);
 	    tabHost.addTab(spec);
+/*
+	    // Do the same for the other tabs
+	    intent = new Intent().setClass(this, AlbumsActivity.class);
+	    spec = tabHost.newTabSpec("albums").setIndicator("Albums",
+	                      res.getDrawable(R.drawable.ic_tab_albums))
+	                  .setContent(intent);
+	    tabHost.addTab(spec);
+*/
+	    //tabHost.setCurrentTab(2);
+	    
+	    testDbHelper();
 	}
 	
 	@Override
@@ -44,6 +61,35 @@ public class MainTabbedActivity extends TabActivity {
 	{
 		super.onDestroy();
 		DatabaseHelper.closeDefaultDb();
+	}
+	
+	private void testDbHelper() {
+		DatabaseHelper dbHelper = DatabaseHelper.getDefaultDb();
+
+		try {
+			dbHelper.createDb();
+		} catch (IOException ioe) {
+			throw new Error("Unable to create database");
+		}
+
+		try {
+			dbHelper.openDb();
+			Hashtable<Long, FarmInfo> infos = dbHelper.getFarmsInRectangle(48.797, 16.8, 49, 17);
+			Log.d("size", infos.size() + "");
+			
+			for (FarmInfo info : infos.values()) {
+				dbHelper.fillDetails(info);
+				
+				Log.d("info detail", info.name + "; " + info.type + "; " + info.description);
+				Log.d("info detail - contact", info.contact.city + "; " + info.contact.street + "; " + info.contact.phoneNumbers.size());
+				Log.d("info detail - categories", "size: " + info.categories.size());
+				Log.d("info detail - products", "size: " + info.products.size());
+			}
+		} catch (SQLException sqle) {
+			throw sqle;
+		} finally {
+			dbHelper.close();
+		}
 	}
 	
 }
